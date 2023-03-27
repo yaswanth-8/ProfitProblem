4f84c2176d61f253794bdaf98787fdb306be6737
yaswanth <ya8wanth@gmail.com>
2023-03-27 16:51:34 +0530

    Add project files.

---
+++ ProfitProblem/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace MyProject;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] priceArray = { 7, 1, 2, 5, 6, 4 };
+        int length=priceArray.Length;
+        int maxProfit = 0;
+        for (int i = 0;i<length;i++)
+        {
+           for(int j=i;j<length;j++) {
+                if (priceArray[j] - priceArray[i] > maxProfit)
+                {
+                    maxProfit = priceArray[j] - priceArray[i];
+                }
+           }
+        }
+        Console.WriteLine(maxProfit);
+    }
+}
